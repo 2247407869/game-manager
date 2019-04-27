67a4c90bd001ede5ddf66b9be7a94a2a77282139
LLS <2247407869@qq.com>
2019-04-27 12:15:54 +0800

    增加了游戏时长统计功能

--- WpfApp1/MainWindow.xaml.cs
+++ WpfApp1/MainWindow.xaml.cs
@@ -6,11 +6,13 @@ using System.Diagnostics;
 using System.IO;
 using System.Linq;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using System.Windows.Data;
 using System.Windows.Documents;
+using System.Windows.Forms;
 using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
@@ -27,12 +29,26 @@ namespace WpfApp1
         List<Game> gameList = new List<Game>();
         ObservableCollection<Game> showdata = new ObservableCollection<Game>();
         string fp = System.Windows.Forms.Application.StartupPath + "\\info.json";
+        int _rowIndex = 0;
+        int playTimeCount = 0;
+        private System.Timers.Timer timer = new System.Timers.Timer();
         public MainWindow()
         {
             InitializeComponent();
+            timerInit();
             readGameList();
             showGameList();
-            //MessageBox.Show("游戏的路径是：" + obji[0].route , "提示", MessageBoxButton.OKCancel, MessageBoxImage.Question);
+        }
+        public void timerInit()
+        {
+            //设置timer可用
+            timer.Enabled = true;
+            //设置timer
+            timer.Interval = 1000;
+            //设置是否重复计时，如果该属性设为False,则只执行timer_Elapsed方法一次。
+            timer.AutoReset = true;
+            timer.Elapsed += new System.Timers.ElapsedEventHandler(timer_Elapsed);
+            timer.Stop();
         }
         private void readGameList()
         {
@@ -72,9 +88,7 @@ namespace WpfApp1
         }
         private void deleteGame_button(object sender, RoutedEventArgs e)
         {
-            int _rowIndex = 0;
-            int _columnIndex = 0;
-            if (GetCellXY(gameShowList, ref _rowIndex, ref _columnIndex))
+            if (GetCellXY(gameShowList, ref _rowIndex))
             {
                 gameList.RemoveAt(_rowIndex);
             }
@@ -83,29 +97,59 @@ namespace WpfApp1
         }
         private void startGame_button(object sender, RoutedEventArgs e)
         {
-            int _rowIndex = 0;
-            int _columnIndex = 0;
-            if (GetCellXY(gameShowList, ref _rowIndex, ref _columnIndex))
+            if (GetCellXY(gameShowList, ref _rowIndex))
             {
+                playTimeCount = gameList[_rowIndex].playTime;
+
                 string route = gameList[_rowIndex].route;
-                Process.Start(route);
+                Process proc = Process.Start(route);
+                proc.EnableRaisingEvents = true;
+                proc.Exited += new EventHandler(gameExit);
+
+                timer.Start();
             }
         }
-        private bool GetCellXY(DataGrid gameShowList, ref int rowIndex, ref int columnIndex)
+        void gameExit(object sender, EventArgs e)
+        {
+            timer.Stop();
+            gameList[_rowIndex].playTime = playTimeCount;
+            saveGameList();
+            System.Windows.MessageBox.Show(gameList[_rowIndex].name+"外部程序已经退出！");
+        }
+        private bool GetCellXY(System.Windows.Controls.DataGrid gameShowList, ref int rowIndex)
         {
             var _cells = gameShowList.SelectedCells;
             if (_cells.Any())
             {
                 rowIndex = gameShowList.Items.IndexOf(_cells.First().Item);
-                columnIndex = _cells.First().Column.DisplayIndex;
                 return true;
             }
             return false;
+        }
+        private void timer_Elapsed(object sender, System.Timers.ElapsedEventArgs e)
+        {
+            playTimeCount += 1;
+            SetTB(playTimeCount);
+        }
+        private void SetTB(int value)
+        {
+            ThreadPool.QueueUserWorkItem(delegate
+            {
+                System.Threading.SynchronizationContext.SetSynchronizationContext(new
+                    System.Windows.Threading.DispatcherSynchronizationContext(System.Windows.Application.Current.Dispatcher));
+                System.Threading.SynchronizationContext.Current.Post(pl =>
+                {
+                    //里面写真正的业务内容
+                    gameList[_rowIndex].playTime = value;
+                    showGameList();
+                }, null);
+            });
         }
     }
     public class Game
     {
         public string name { get; set; }
         public string route { get; set; }
+        public int playTime { get; set; }
     }
 }
